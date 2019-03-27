b16db560a71f59441fd8d6ac68af346b34ac4d48
highslater <highslater@hotmail.com>
2019-03-27 01:23:22 -0400

    commit_06

--- 06_Tutorial_Property_Decorators.py
+++ 06_Tutorial_Property_Decorators.py
@@ -32,128 +32,77 @@ print(version_info) if PRINT_VERSION_INFO else None
 class Employee():
     """docstring for Employee."""
 
-    # class variables.
-    number_of_employees = 0
-    raise_amount = 1.04
-
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last):
         """Docstring."""
         self.first = first
         self.last = last
-        self.pay = pay
-        self.email = first + '.' + last + '@company.com'
 
-        Employee.number_of_employees += 1
-
-    def __repr__(self):
+    @property
+    def email(self):
         """Docstring."""
-        return "Employee('{}', '{}', '{}')".format(
-            self.first, self.last, self.pay)
+        return '{}.{}@email.com'.format(self.first, self.last)
 
-    def __str__(self):
-        """Docstring."""
-        return '{} - {}'.format(self.full_name(), self.email)
-
-    def __add__(self, other):
-        """Docstring."""
-        return self.pay + other.pay
-
-    def __len__(self):
-        """Docstring."""
-        return len(self.full_name())
-
+    @property
     def full_name(self):
         """Docstring."""
         return '{} {}'.format(self.first, self.last)
 
-    def apply_raise(self):
+    @full_name.setter
+    def full_name(self, name):
         """Docstring."""
-        self.pay = int(self.pay * self.raise_amount)
+        first, last = name.split(" ")
+        self.first = first
+        self.last = last
 
-    @classmethod
-    def set_raise_amount(cls, amount):
+    @full_name.deleter
+    def full_name(self):
         """Docstring."""
-        cls.raise_amount = amount
-
-    @classmethod
-    def from_string(cls, emp_str):
-        """Docstring."""
-        first, last, pay = emp_str.split("-")
-        return cls(first, last, pay)
-
-    @staticmethod
-    def is_workday(day):
-        """Docstring."""
-        return day.weekday() in range(5)
+        print(" Delete Name!\n")
+        self.first = None
+        self.last = None
 
 
-class Developer(Employee):
-    """Docstring for Developer."""
+emp_1 = Employee('John', 'Smith')
 
-    raise_amount = 1.10
+print('', "-" * 88, '\n')
+print('', emp_1.first)
+print('', emp_1.last)
+print('', emp_1.email)
+print('', emp_1.full_name)
+print('\n', "-" * 88, '\n')
 
-    def __init__(self, first, last, pay, prog_lang):
-        """Docstring."""
-        super().__init__(first, last, pay)
-        self.prog_lang = prog_lang
+emp_1.first = 'Jim'
 
+print('', "-" * 88, '\n')
+print('', emp_1.first)
+print('', emp_1.last)
+print('', emp_1.email)
+print('', emp_1.full_name)
+print('\n', "-" * 88, '\n')
 
-class Manager(Employee):
-    """Docstring for Manager."""
+emp_1 = Employee('John', 'Smith')
 
-    def __init__(self, first, last, pay, employees=None):
-        """Docstring."""
-        super().__init__(first, last, pay)
-        if employees is None:
-            self.employees = []
-        else:
-            self.employees = employees
+print('', "-" * 88, '\n')
+print('', emp_1.first)
+print('', emp_1.last)
+print('', emp_1.email)
+print('', emp_1.full_name)
+print('\n', "-" * 88, '\n')
 
-    def add_employee(self, emp):
-        """Docstring."""
-        if emp not in self.employees:
-            self.employees.append(emp)
+emp_1.full_name = 'Corey Schafer'
 
-    def remove_employee(self, emp):
-        """Docstring."""
-        if emp in self.employees:
-            self.employees.remove(emp)
+print('', "-" * 88, '\n')
+print('', emp_1.first)
+print('', emp_1.last)
+print('', emp_1.email)
+print('', emp_1.full_name)
+print('\n', "-" * 88, '\n')
 
-    def print_employees(self):
-        """Docstring."""
-        for emp in self.employees:
-            print('-->', emp.full_name())
+del emp_1.full_name
 
-
-emp_1 = Developer('Corey', 'Schafer', 50000, 'Python')
-emp_2 = Developer('Test', 'Employee', 60000, 'Java')
-
-print('', "-" * 88, "\n")
-print('', emp_1)
-print('\n', "-" * 88, "\n")
-
-print('', "-" * 88, "\n")
-print('', repr(emp_1))
-print('\n', "-" * 88, "\n")
-
-print('', "-" * 88, "\n")
-print('', emp_1.__str__())
-print('\n', "-" * 88, "\n")
-
-print('', "-" * 88, "\n")
-print('', emp_1.__repr__())
-print('\n', "-" * 88, "\n")
-
-print('', "-" * 88, "\n")
-print(emp_1 + emp_2)
-print('\n', "-" * 88, "\n")
-
-print('', "-" * 88, "\n")
-print(len('four'))
-print('four'.__len__())
-print('\n', "-" * 88, "\n")
-
-print('', "-" * 88, "\n")
-print(len(emp_1))
-print(len(emp_2))
-print('\n', "-" * 88, "\n")
+print('', "-" * 88, '\n')
+print('', emp_1.first)
+print('', emp_1.last)
+print('', emp_1.email)
+print('', emp_1.full_name)
+print('\n', "-" * 88, '\n')
