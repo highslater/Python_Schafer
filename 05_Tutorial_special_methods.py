a0300affc5ff457073fa13fd5987574f20f1e72f
highslater <highslater@hotmail.com>
2019-03-27 00:45:27 -0400

    commit_05

--- 05_Tutorial_special_methods.py
+++ 05_Tutorial_special_methods.py
@@ -45,6 +45,23 @@ class Employee():
 
         Employee.number_of_employees += 1
 
+    def __repr__(self):
+        """Docstring."""
+        return "Employee('{}', '{}', '{}')".format(
+            self.first, self.last, self.pay)
+
+    def __str__(self):
+        """Docstring."""
+        return '{} - {}'.format(self.full_name(), self.email)
+
+    def __add__(self, other):
+        """Docstring."""
+        return self.pay + other.pay
+
+    def __len__(self):
+        """Docstring."""
+        return len(self.full_name())
+
     def full_name(self):
         """Docstring."""
         return '{} {}'.format(self.first, self.last)
@@ -108,50 +125,35 @@ class Manager(Employee):
             print('-->', emp.full_name())
 
 
-dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
-dev_2 = Developer('Test', 'Employee', 60000, 'Java')
+emp_1 = Developer('Corey', 'Schafer', 50000, 'Python')
+emp_2 = Developer('Test', 'Employee', 60000, 'Java')
 
-mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
-mgr_1.add_employee(dev_2)
+print('', "-" * 88, "\n")
+print('', emp_1)
+print('\n', "-" * 88, "\n")
 
-print("", "-" * 88, "\n")
-print(dev_1.email)
-print(dev_2.email)
-print("\n", "-" * 88)
+print('', "-" * 88, "\n")
+print('', repr(emp_1))
+print('\n', "-" * 88, "\n")
 
-print("", "-" * 88, "\n")
-print(dev_1.pay)
-dev_1.apply_raise()
-print(dev_1.pay)
-print("\n", "-" * 88)
+print('', "-" * 88, "\n")
+print('', emp_1.__str__())
+print('\n', "-" * 88, "\n")
 
-print("", "-" * 88, "\n")
-print(dev_1.email)
-print(dev_1.prog_lang)
-print("\n", "-" * 88)
+print('', "-" * 88, "\n")
+print('', emp_1.__repr__())
+print('\n', "-" * 88, "\n")
 
-print("", "-" * 88, "\n")
-print(mgr_1.full_name())
-print(mgr_1.email)
-print(mgr_1.pay)
-mgr_1.print_employees()
-print("\n", "-" * 88)
+print('', "-" * 88, "\n")
+print(emp_1 + emp_2)
+print('\n', "-" * 88, "\n")
 
-mgr_1.remove_employee(dev_1)
+print('', "-" * 88, "\n")
+print(len('four'))
+print('four'.__len__())
+print('\n', "-" * 88, "\n")
 
-print("", "-" * 88, "\n")
-print(mgr_1.full_name())
-print(mgr_1.email)
-print(mgr_1.pay)
-mgr_1.print_employees()
-print("\n", "-" * 88)
-
-print("", "-" * 88, "\n")
-print(isinstance(mgr_1, Employee))
-print(isinstance(mgr_1, Manager))
-print(isinstance(mgr_1, Developer))
-
-print(issubclass(Developer, Employee))
-print(issubclass(Manager, Manager))
-print(issubclass(Manager, Developer))
-print("\n", "-" * 88)
+print('', "-" * 88, "\n")
+print(len(emp_1))
+print(len(emp_2))
+print('\n', "-" * 88, "\n")
